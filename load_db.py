1bd17abfec020941cbdeae2bbc09518058189c71
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-04-24 18:57:22 +0100

    set up app structure

---
+++ load_db.py
@@ -0,0 +1,8 @@
+import pdb
+# from models.location import Location
+# from models.user import User
+# from models.visit import Visit
+
+# import repositories.location_repository as location_repository
+# import repositories.user_repository as user_repository
+# import repositories.visit_repository as visit_repository
