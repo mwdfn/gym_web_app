baca2068be7da53d8f5b385fbbf156c8d6eb9950
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-04-25 14:40:08 +0100

    html gym app header made

--- controllers/exercise_class_controller.py
+++ controllers/exercise_class_controller.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.exercise_class import ExerciseClass
+import repositories.exercise_class_repository as exercise_class_repository
 
 exercise_classes_blueprint = Blueprint("exercise_classes", __name__)
 
+
+@exercise_classes_blueprint.route("/exercise-classes")
+def exercise_classes():
+    exercise_classes = exercise_class_repository.select_all()
+    return render_template("exercise-classes/index.html", exercise_classes=exercise_classes)
 
